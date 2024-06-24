1726199bf0ba28bc00ee8668f5749207c32ff9f1
Harsh5784 <harshbha30@gmail.comq>
2024-06-24 23:17:02 +0530

    datatypes in js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const score = 100;
+const isloggedin = false;
+const id = Symbol('123');
+const i = Symbol('123');
+
+console.log(id===i);
+
+const arra= ["SHaktiman","Pokemon"];
+
 let obj = {
-    name:"Harsh Bhasin",
-    Course : "BCA"
-}
+    name: "Harsh",
+    age: 20,
+    country: "India",
+};
 
-let jsinjson = JSON.stringify(obj);
-
-console.log(jsinjson);
-
-let jsoninjs = JSON.parse(jsinjson);
-
-console.log(jsoninjs);
